bf49a80a4e066ad24cd85a64ab8309fd4ba5ba05
TominoLP <erfinder.tom@gmail.com>
2023-11-28 14:43:53 +0100

    Initial commit

---
+++ src/main/java/de/tom/struktogen/StruktoGenApplication.java
@@ -0,0 +1,13 @@
+package de.tom.struktogen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StruktoGenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StruktoGenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/tom/struktogen/StruktoGenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tom.struktogen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StruktoGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

c15a5c2a6988f1c467e1b334d256d70f1ec07580
Tom Werth <twe@netlands.de>
2023-11-29 11:13:47 +0100

    removed old code

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,20 +1,8 @@
 <script setup>
-import Assignment from './components/Assignment.vue'
-
-const elements = [
-  { variableName: 'x', expression: '1 + 2' },
-  { variableName: 'y', expression: 'x + 3' },
-  { variableName: 'z', expression: 'y + 4' }
-]
-
 </script>
 
 
 <template>
-
-  <div id="app">
-    <Assignment v-for="element in elements" :element="element" :key="element.variableName" />
-  </div>
 
 </template>
 
